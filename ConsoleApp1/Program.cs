b63ef649659787229d3b9598372e2a66425820cd
Mayu <netgmayu@gmail.com>
2021-12-06 02:26:46 +0530

    Commit 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+calculator calc = new calculator();
+int result = calc.Add();
+calc.x = 10;
+calc.y = 20;
+Console.WriteLine(result);
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Calculator
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public int Add()
+    {
+        return x + y;
+    }
+    public int Subtract()
+    {
+        return y - x;
+    }
+
+}
